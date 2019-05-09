e32c9f436cb4e32a77134fa21cd7e3f67d9f0db3
arturjoshi <rG#d*kz%3Hf1P>
2019-05-09 18:56:21 +0300

    refactoring

--- SpringBootProject/src/main/java/com/policePlatform/rest/HealthController.java
+++ SpringBootProject/src/main/java/com/policePlatform/rest/HealthController.java
@@ -1,4 +1,4 @@
-package com.policePlatform;
+package com.policePlatform.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HealthController {
 
     @GetMapping("/api/v1/health")
-    public void getHealthController()
+    public void health()
     {
 
     }
