e3446abff818a0a80b8707379bd6b48e177bdd32
arturjoshi <rG#d*kz%3Hf1P>
2019-07-24 21:58:18 +0300

    [PP-9] - fixed imports

--- SpringBootProject/src/main/java/com/policePlatform/api/rest/PoliceEmployeeController.java
+++ SpringBootProject/src/main/java/com/policePlatform/api/rest/PoliceEmployeeController.java
@@ -1,16 +1,24 @@
 package com.policePlatform.api.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.policePlatform.api.rest.dto.PoliceEmployeeRequest;
 import com.policePlatform.api.rest.dto.PoliceEmployeeResponse;
 import com.policePlatform.api.rest.dto.PoliceEmployeeSearchRequest;
 import com.policePlatform.services.PoliceEmployeeService;
-import com.policePlatform.services.PoliceReportService;
+
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/police-employee")
@@ -35,7 +43,7 @@ public class PoliceEmployeeController {
 
     @PutMapping("/{id}")
     public PoliceEmployeeResponse updatePoliceEmployee(@PathVariable("id") Long id,
-                                                   @RequestBody PoliceEmployeeRequest request) {
+                                                       @RequestBody PoliceEmployeeRequest request) {
         return policeEmployeeServise.updatePoliceEmployee(id, request);
     }
 
